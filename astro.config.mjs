6f532ddb0c03dd08adbf175c7173807b8ffe5dad
Gabriel Gamez <gabrielgamezdev@gmail.com>
2026-05-14 00:59:27 -0400

    feat: remove base path for user site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://gabrielgamezdev.github.io',
-	base: '/portfolio',
 	output: 'static',
 	redirects: {
 		'/es/': '/',
